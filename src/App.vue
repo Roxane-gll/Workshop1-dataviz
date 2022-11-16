0272a476eaf0ab8f538377f71e6eab40feb2414d
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2022-11-16 11:27:47 +0100

    adding img and style, completing json

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,42 @@
 <template>
   <div id="app">
-    <button @click="changePage(-1)">←</button>
-    <button @click="changePage(1)">→</button>
+    <!--<button @click="changePage(-1)">←</button>
+    <button @click="changePage(1)">→</button>-->
     <component :story="getCurrentStory" :is="getCurrentPage"></component>
 
-    <template v-for="story in allStories">
-      <button :key="story.id" @click="changeStory(story.id)">{{story.name}}</button>
-    </template>
+    <div id="choose-children-wrapper">
+
+      <template v-for="story in allStories">
+
+        <div class="children-wrapper" :key="story.id" @click="changeStory(story.id)">
+
+          <img :src="require(`./assets/img/${story.style.backgroundChooseChildren}`)" alt="background Children" class="backgroundChooseChildren">
+
+          <img :src="require(`./assets/img/${story.style.iconChildren}`)" alt="Icon of the children" class="iconChildren">
+
+          <div class="info-children">
+
+            <h5 class="history">L'histoire de</h5>
+
+            <h2 class="name">{{ story.name }}</h2>
+
+            <div class="country-wrapper">
+
+              <img :src="require(`./assets/img/${story.style.arrowChildren}`)" alt="Arrow of the children">
+
+              <h3 class="country">{{ story.country }}</h3>
+
+            </div>
+
+
+          </div>
+
+        </div>
+
+      </template>
+
+    </div>
+
   </div>
 </template>
 
@@ -51,6 +81,8 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="css">
+
+@import './assets/scss/style.css';
 
 </style>
