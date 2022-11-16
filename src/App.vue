f5d1f55c4c28c618339dd1ff7735ed33cd1b09a1
Roxane-gll <guella.roxane@gmail.com>
2022-11-16 10:12:42 +0100

    Add Start form and Story selector component

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,14 @@
 <template>
   <div id="app">
-    <!--<button @click="changePage(-1)">←</button>
-    <button @click="changePage(1)">→</button>-->
-    <component :story="getCurrentStory" :is="getCurrentPage"></component>
-
-    <div id="choose-children-wrapper">
-
-      <template v-for="story in allStories">
-
-        <div class="children-wrapper" :key="story.id" @click="changeStory(story.id)">
-
-          <img :src="require(`./assets/img/${story.style.backgroundChooseChildren}`)" alt="background Children" class="backgroundChooseChildren">
-
-          <img :src="require(`./assets/img/${story.style.iconChildren}`)" alt="Icon of the children" class="iconChildren">
-
-          <div class="info-children">
-
-            <h5 class="history">L'histoire de</h5>
-
-            <h2 class="name">{{ story.name }}</h2>
-
-            <div class="country-wrapper">
-
-              <img :src="require(`./assets/img/${story.style.arrowChildren}`)" alt="Arrow of the children">
-
-              <h3 class="country">{{ story.country }}</h3>
-
-            </div>
-
-
-          </div>
-
-        </div>
-
-      </template>
-
-    </div>
-
+    <component :story="currentStory" :is="getCurrentComponent" @newStory="changeStory" @startForm="saveForm"></component>
   </div>
 </template>
 
 <script>
-import Stories from './components/Stories.vue'
 import allStories from './assets/stories.json'
-import Graph from './components/Graph.vue'
+import BaseStory from './components/BaseStory.vue'
+import StorySelector from './components/StorySelector.vue'
+import StartForm from './components/StartQuestion.vue'
 
 export default {
   name: 'App',
@@ -51,31 +16,28 @@ export default {
     return {
       allStories,
       currentStory: 1,
-      currentPage: 1,
-      pages: {
-        1: Stories,
-        2: Graph
-      }
+      currentComponent: 1,
+      components: {
+        1: StartForm,
+        2: StorySelector,
+        3: BaseStory
+      },
+      startForm: {}
     }
   },
   computed: {
-    getCurrentPage() {
-      return this.pages[this.currentPage]
-    },
-    getCurrentStory() {
-      return this.allStories.find((story) => story.id === this.currentStory)
+    getCurrentComponent() {
+      return this.components[this.currentComponent]
     }
   },
   methods: {
     changeStory(newStoryId) {
-      this.currentPage = 1
       this.currentStory = newStoryId
+      this.currentComponent = 3
     },
-    changePage(newPage) {
-      if (!this.pages[this.currentPage + newPage]) {
-        return
-      }
-      this.currentPage += newPage
+    saveForm(model) {
+      this.startForm = model
+      this.currentComponent = 2
     }
   }
 }
---
+++ src/components/StartQuestion.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+      <form>
+          <div v-for="input in inputs" :key="input.name">
+              <label :for="input.name">{{ input.name }}</label>
+              <input :name="input.name" v-model="input.model">
+          </div>
+          <button @click="saveForm">Save</button>
+      </form>
+  </div>
+</template>
+
+<script>
+import inputs from '../assets/startForm.json'
+
+export default {
+  name: 'StartForm',
+  props: {
+      story: {
+          type:Number
+      }
+  },
+  data() {
+    return {
+      inputs,
+      models: {
+          q1: null,
+          q2: null
+      }
+    }
+  },
+  methods: {
+    saveForm() {
+        this.$emit('startForm', this.models)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
