cc767f609c3ead71e1447ea7d1e750ebe131f3b7
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2022-11-18 15:01:24 +0100

    fix responsive part 14

--- src/components/pages/graphStory/GraphStoryFigg.vue
+++ src/components/pages/graphStory/GraphStoryFigg.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="hello">
-    <div style="display:flex;">
-          <div v-for="(values, key) in story.graph.g1" :key="key" @click="switchCountry(key)">
-            {{key}}
-          </div>
-        </div>
 
-        <div>
-            <img :src="require('@/assets/img/schoolImg/teacher.svg')">
-        </div>
+    <div class="button-wrapper">
+      <div :class="gaphCountry === key ? 'button active' : 'button'" v-for="(values, key) in story.graph.g1" :key="key"
+           @click="switchCountry(key)">
+        {{ key }}
+      </div>
+    </div>
 
-        <div>
-          <img v-for="(value, index) in story.graph.g1[gaphCountry]" :key="index" :src="require(`@/assets/img/schoolImg/${value}.svg`)" class="imgSchool">
-        </div>
+    <hr>
 
-        <p>En {{gaphCountry}}, un professeur donne cours à {{story.graph.g1[gaphCountry].length}} élèves en moyenne.</p>
+    <div class="data-wrapper">
+
+      <img class="teacher" :src="require('@/assets/img/schoolImg/teacher.svg')" alt="teacher">
+
+      <div class="students-wrapper">
+        <img v-for="(value, index) in story.graph.g1[gaphCountry]" :key="index" :src="require(`@/assets/img/schoolImg/${value}.svg`)" class="imgSchool" alt="students">
+      </div>
+
+    </div>
+
+    <p class="phase">En {{ gaphCountry }}, un professeur donne cours à {{ story.graph.g1[gaphCountry].length }} élèves
+      en moyenne.</p>
+
   </div>
 </template>
 
@@ -22,7 +30,7 @@
 export default {
   name: 'GraphStoryFigg',
   props: {
-    story:{
+    story: {
       type: Object
     }
   },
@@ -36,6 +44,6 @@ export default {
       this.gaphCountry = country
     }
   }
-  
+
 }
 </script>
